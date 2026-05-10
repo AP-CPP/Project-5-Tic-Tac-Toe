55c09ad80db0328ccf9dece8a4b499f782b2cdf8
Andrew <ajpahomi@cpp.edu>
2026-05-10 16:16:06 -0700

    Define winning conditions for tic-tac-toe and changed file name
    Added winning conditions for tic-tac-toe game.

--- game.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-#define BOARD_SiZE 9
-#define EMPTY ' '
---
+++ tictactoe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "game.h"
+
+static const int WIN_CONDITIONS[8][3]{
+  {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+  {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+  {0, 4, 8}, {2, 4, 6}
+}; /* TOtal of 8 possible ways to win tic-tac-toe
+      first set are the rows then columns and the last two are the diagonals*/
