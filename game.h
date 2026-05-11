eeac1ba52227c1290a8853ac5fce372ce1c6540d
Andrew <ajpahomi@cpp.edu>
2026-05-10 17:23:31 -0700

    Add function declarations for game logic
    Added function declarations for label conversion and game result checking.

--- game.h
+++ game.h
@@ -5,3 +5,9 @@
 typedef enum {
     RESULT_NONE, RESULT_X_WIN, RESULT_O_WIN, RESULT_DRAW
 } GameResult;
+
+
+int  label_to_index(const char *label); // Used for converting the lables in the directions "A1" into 1 for the array index
+void index_to_label(int idx, char *out); // Opposite of the previous function
+void available_positions(const char *board, char *out); // Just used for seprating commas 
+GameResult check_result(const char *board); // Checking the game outcome
