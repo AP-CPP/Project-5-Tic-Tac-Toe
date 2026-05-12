f1c18f2579768434bb65a111872808b3b8e7cd2f
Andrew <ajpahomi@cpp.edu>
2026-05-11 17:27:46 -0700

    Add MQTT variables and message handler

--- gui.c
+++ gui.c
@@ -23,3 +23,19 @@ typedef enum {
     SCREEN_MODE,
     SCREEN_GAME
 } Screen;
+
+static char g_url[128] = "apcpp.duckdns.org";
+static int  g_url_len  = 17; 
+static const char *g_mqtt_pass = NULL;
+static struct mosquitto *g_mosq = NULL;
+static int g_mqtt_connected = 0;
+static char g_status_text[64] = "";
+
+
+static void on_message(struct mosquitto *m, void *obj, const struct mosquitto_message *msg) {
+    (void)m; (void)obj;
+char buf[128];
+    int n = msg->payloadlen < (int)sizeof(buf) - 1 ? msg->payloadlen : (int)sizeof(buf) - 1;
+    memcpy(buf, msg->payload, n);
+    buf[n] = '\0';
+
